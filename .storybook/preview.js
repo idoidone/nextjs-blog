45ae3b2cf930fb90923a76f14320e89fd257e93d
Nhan D Le <code.nhan@gmail.com>
2021-10-19 11:33:07 +0700

    Init Storybook for design system

---
+++ .storybook/preview.js
@@ -0,0 +1,19 @@
+import * as nextImage from 'next/image';
+
+
+// Workaround Next.js Image external source
+// hostname "source" is not configured under images in your `next.config.js
+Object.defineProperty(nextImage, 'default', {
+  configurable: true,
+  value: props => <img {...props} />
+});
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
---
+++ components/Image.stories.tsx
@@ -0,0 +1,8 @@
+import Image from 'next/image'
+
+export default {
+    component: Image,
+    title: 'NextJS/Image'
+}
+
+export const Default = () => <Image src="https://avatars.githubusercontent.com/u/1247539" />
